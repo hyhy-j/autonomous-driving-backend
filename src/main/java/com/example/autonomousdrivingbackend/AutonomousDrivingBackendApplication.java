6c85bef1d4b4f2a9cc8ecd57e6fea5754e4de460
hyhy-j <lhjlhj10088@gmail.com>
2026-04-06 11:41:23 +0900

    settings

---
+++ src/main/java/com/example/autonomousdrivingbackend/AutonomousDrivingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.autonomousdrivingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonomousDrivingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutonomousDrivingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autonomousdrivingbackend/AutonomousDrivingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autonomousdrivingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonomousDrivingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
